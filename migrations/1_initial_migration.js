6ccaeef4b9c7a0ff8132f28f9304ab3eded78543
GabrielGhimici <gabriel.ghimici@teads.tv>
2019-09-10 03:19:39 +0300

    save address + abi

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,50 @@
 const Migrations = artifacts.require("Migrations");
+const mysql      = require('mysql');
+const dotenv     = require('dotenv');
+
+dotenv.config({ path: '../.env' });
+
+const connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  port     : process.env.DB_PORT,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASS,
+  database : process.env.DB_DATABASE
+});
+
+const contractName = 'initial_migration';
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Migrations).then(function() {
+    connection.connect();
+    connection.query('select * from contract where name=?',[contractName], function (error, results) {
+      if (error) throw error;
+      if (results.length === 0) {
+        const contract = {
+          name: contractName,
+          address: Migrations.address,
+          abi: JSON.stringify(Migrations.abi)
+        }
+        connection.query('insert into contract set ?', contract, function (error, results) {
+          if (error) throw error;
+          if (results.insertId > 0) {
+            console.log('Contract info for ', contractName, ' was added to ', results.insertId);
+          }
+          connection.end();
+        })
+      } else {
+        connection.query(
+          'update contract set address = ?, abi = ?, updatedAt = CURRENT_TIMESTAMP where name = ?',
+          [Migrations.address, JSON.stringify(Migrations.abi), contractName], 
+          function (error, results) {
+            if (error) throw error;
+            if (results.changedRows > 0) {
+              console.log('Contract info for ', contractName, ' was updated.');
+            }
+            connection.end();
+          }
+        )
+      }
+    });
+  });
 };
--- migrations/2_owner_migration.js
+++ migrations/2_owner_migration.js
@@ -1,5 +1,50 @@
 const Owned = artifacts.require("Owned");
+const mysql      = require('mysql');
+const dotenv     = require('dotenv');
+
+dotenv.config({ path: '../.env' });
+
+const connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  port     : process.env.DB_PORT,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASS,
+  database : process.env.DB_DATABASE
+});
+
+const contractName = 'owned_migration';
 
 module.exports = function(deployer) {
-  deployer.deploy(Owned);
+  deployer.deploy(Owned).then(function() {
+    connection.connect();
+    connection.query('select * from contract where name=?',[contractName], function (error, results) {
+      if (error) throw error;
+      if (results.length === 0) {
+        const contract = {
+          name: contractName,
+          address: Owned.address,
+          abi: JSON.stringify(Owned.abi)
+        }
+        connection.query('insert into contract set ?', contract, function (error, results) {
+          if (error) throw error;
+          if (results.insertId > 0) {
+            console.log('Contract info for ', contractName, ' was added to ', results.insertId);
+          }
+          connection.end();
+        })
+      } else {
+        connection.query(
+          'update contract set address = ?, abi = ?, updatedAt = CURRENT_TIMESTAMP where name = ?',
+          [Owned.address, JSON.stringify(Owned.abi), contractName], 
+          function (error, results) {
+            if (error) throw error;
+            if (results.changedRows > 0) {
+              console.log('Contract info for ', contractName, ' was updated.');
+            }
+            connection.end();
+          }
+        )
+      }
+    });
+  });
 };
